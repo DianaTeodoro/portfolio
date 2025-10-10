15e91e914c443f39b41569fff96d192c1af6c910
DianaTeodoro <dianateodoro992@gmail.com>
2025-10-10 13:45:38 +0200

    added responsive

---
+++ desktop/data/story.js
@@ -0,0 +1,943 @@
+var story = {
+ "docName": "Portfolio-Diana-Teodoro",
+ "docPath": "P_P_P",
+ "docVersion": 100000001,
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "fileType": "png",
+ "disableInteractions": false,
+ "highlightHotspot": false,
+ "highlightAllHotspots": false,
+ "hideGallery": true,
+ "galleryPageColorsEnabled": false,
+ "galleryDecorNodesEnabled": false,
+ "zoomEnabled": true,
+ "cloud": false,
+ "singleFile": false,
+ "title": "Portfolio-Diana-Teodoro",
+ "layersExist": false,
+ "pages": [
+  {
+   "id": "1:18",
+   "groupIndex": 0,
+   "title": "1. Landing",
+   "image": "1.-landing.png",
+   "index": 0,
+   "width": 1440,
+   "height": 822,
+   "x": 4571,
+   "y": 0,
+   "awidth": 1440,
+   "aheight": 822,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Diana Teodoro",
+     "rect": {
+      "x": 360,
+      "y": 0,
+      "width": 360,
+      "height": 85
+     },
+     "index": 0,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 1,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Diana Teodoro",
+     "rect": {
+      "x": 720,
+      "y": 0,
+      "width": 360,
+      "height": 85
+     },
+     "index": 1,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Diana Teodoro",
+     "rect": {
+      "x": 1080,
+      "y": 0,
+      "width": 360,
+      "height": 85
+     },
+     "index": 2,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 6,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "1:36",
+   "groupIndex": 0,
+   "title": "2. My Design Principles",
+   "image": "2.-my-design-principles.png",
+   "index": 1,
+   "width": 1440,
+   "height": 5302,
+   "x": 6328,
+   "y": 0,
+   "awidth": 1440,
+   "aheight": 5302,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Footer",
+     "rect": {
+      "x": 0,
+      "y": 5072,
+      "width": 1440,
+      "height": 230
+     },
+     "index": 3,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Diana Teodoro",
+     "rect": {
+      "x": 0,
+      "y": 0,
+      "width": 360,
+      "height": 85
+     },
+     "index": 4,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Diana Teodoro",
+     "rect": {
+      "x": 720,
+      "y": 0,
+      "width": 360,
+      "height": 85
+     },
+     "index": 5,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Diana Teodoro",
+     "rect": {
+      "x": 1080,
+      "y": 0,
+      "width": 360,
+      "height": 85
+     },
+     "index": 6,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 6,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "1:894",
+   "groupIndex": 0,
+   "title": "3. Recent Projects",
+   "image": "3.-recent-projects.png",
+   "index": 2,
+   "width": 1440,
+   "height": 839,
+   "x": 8499,
+   "y": 0,
+   "awidth": 1440,
+   "aheight": 839,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Qualitatively, Blendy.",
+     "rect": {
+      "x": 636,
+      "y": 174,
+      "width": 805,
+      "height": 70
+     },
+     "index": 7,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Content Discovery",
+     "rect": {
+      "x": 361,
+      "y": 405,
+      "width": 718,
+      "height": 70
+     },
+     "index": 8,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 4,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Designing for TV",
+     "rect": {
+      "x": -4,
+      "y": 650,
+      "width": 641,
+      "height": 70
+     },
+     "index": 9,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Diana Teodoro",
+     "rect": {
+      "x": 0,
+      "y": 0,
+      "width": 360,
+      "height": 85
+     },
+     "index": 10,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Diana Teodoro",
+     "rect": {
+      "x": 360,
+      "y": 0,
+      "width": 360,
+      "height": 85
+     },
+     "index": 11,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 1,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Diana Teodoro",
+     "rect": {
+      "x": 1080,
+      "y": 0,
+      "width": 360,
+      "height": 85
+     },
+     "index": 12,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 6,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "1:86",
+   "groupIndex": 0,
+   "title": "4. Blendy",
+   "image": "4.-blendy.png",
+   "index": 3,
+   "width": 1442,
+   "height": 9144,
+   "x": 10224,
+   "y": 0,
+   "awidth": 1442,
+   "aheight": 9144,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Next Project",
+     "rect": {
+      "x": 1,
+      "y": 8805,
+      "width": 1440,
+      "height": 109
+     },
+     "index": 13,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 4,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Footer",
+     "rect": {
+      "x": 1,
+      "y": 8914,
+      "width": 1440,
+      "height": 230
+     },
+     "index": 14,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Diana Teodoro",
+     "rect": {
+      "x": 1,
+      "y": 1,
+      "width": 360,
+      "height": 85
+     },
+     "index": 15,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Diana Teodoro",
+     "rect": {
+      "x": 361,
+      "y": 1,
+      "width": 360,
+      "height": 85
+     },
+     "index": 16,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 1,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Diana Teodoro",
+     "rect": {
+      "x": 721,
+      "y": 1,
+      "width": 360,
+      "height": 85
+     },
+     "index": 17,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Diana Teodoro",
+     "rect": {
+      "x": 1081,
+      "y": 1,
+      "width": 360,
+      "height": 85
+     },
+     "index": 18,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 6,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "1:186",
+   "groupIndex": 0,
+   "title": "5. Content Discovery",
+   "image": "5.-content-discovery.png",
+   "index": 4,
+   "width": 1440,
+   "height": 8064,
+   "x": 11914,
+   "y": 0,
+   "awidth": 1440,
+   "aheight": 8064,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Next Project",
+     "rect": {
+      "x": 0,
+      "y": 7724.12158203125,
+      "width": 1440,
+      "height": 109
+     },
+     "index": 19,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Footer",
+     "rect": {
+      "x": 0,
+      "y": 7833.12158203125,
+      "width": 1440,
+      "height": 230
+     },
+     "index": 20,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Diana Teodoro",
+     "rect": {
+      "x": 0,
+      "y": 0,
+      "width": 360,
+      "height": 85
+     },
+     "index": 21,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Diana Teodoro",
+     "rect": {
+      "x": 360,
+      "y": 0,
+      "width": 360,
+      "height": 85
+     },
+     "index": 22,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 1,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Diana Teodoro",
+     "rect": {
+      "x": 720,
+      "y": 0,
+      "width": 360,
+      "height": 85
+     },
+     "index": 23,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Diana Teodoro",
+     "rect": {
+      "x": 1080,
+      "y": 0,
+      "width": 360,
+      "height": 85
+     },
+     "index": 24,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 6,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "1:796",
+   "groupIndex": 0,
+   "title": "Designing for TV",
+   "image": "designing-for-tv.png",
+   "index": 5,
+   "width": 1440,
+   "height": 7204,
+   "x": 13640,
+   "y": 0,
+   "awidth": 1440,
+   "aheight": 7204,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Diana Teodoro",
+     "rect": {
+      "x": 0,
+      "y": 2,
+      "width": 360,
+      "height": 85
+     },
+     "index": 25,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Diana Teodoro",
+     "rect": {
+      "x": 360,
+      "y": 2,
+      "width": 360,
+      "height": 85
+     },
+     "index": 26,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 1,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Diana Teodoro",
+     "rect": {
+      "x": 720,
+      "y": 2,
+      "width": 360,
+      "height": 85
+     },
+     "index": 27,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Diana Teodoro",
+     "rect": {
+      "x": 1080,
+      "y": 2,
+      "width": 360,
+      "height": 85
+     },
+     "index": 28,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 6,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Next Project",
+     "rect": {
+      "x": 0,
+      "y": 6865,
+      "width": 1440,
+      "height": 109
+     },
+     "index": 29,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Footer",
+     "rect": {
+      "x": 0,
+      "y": 6974,
+      "width": 1440,
+      "height": 230
+     },
+     "index": 30,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "1:913",
+   "groupIndex": 0,
+   "title": "Contact Me",
+   "image": "contact-me.png",
+   "index": 6,
+   "width": 1440,
+   "height": 839,
+   "x": 15508,
+   "y": 0,
+   "awidth": 1440,
+   "aheight": 839,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Diana Teodoro",
+     "rect": {
+      "x": 0,
+      "y": 0,
+      "width": 360,
+      "height": 85
+     },
+     "index": 31,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 6,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Diana Teodoro",
+     "rect": {
+      "x": 360,
+      "y": 0,
+      "width": 360,
+      "height": 85
+     },
+     "index": 32,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 6,
+       "frameIndex": 1,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Diana Teodoro",
+     "rect": {
+      "x": 720,
+      "y": 0,
+      "width": 360,
+      "height": 85
+     },
+     "index": 33,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 6,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  }
+ ],
+ "startPageIndex": 0,
+ "totalImages": 7,
+ "fileKey": "portfolio10octmobile",
+ "groups": [
+  {
+   "id": "0:1",
+   "index": 0,
+   "name": "Adapted Prototype",
+   "backColor": "#2F2F2F"
+  }
+ ]
+}
