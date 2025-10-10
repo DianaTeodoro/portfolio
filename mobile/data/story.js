2421ac6aaf70f3cf1f1f772f6ef7e6147e159f24
DianaTeodoro <dianateodoro992@gmail.com>
2025-10-10 13:47:50 +0200

    added more views

---
+++ mobile/data/story.js
@@ -0,0 +1,920 @@
+var story = {
+ "docName": "Portfolio-Diana-Teodoro",
+ "docPath": "P_P_P",
+ "docVersion": 100000001,
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "fileType": "png",
+ "disableInteractions": false,
+ "highlightHotspot": false,
+ "highlightAllHotspots": false,
+ "hideGallery": true,
+ "galleryPageColorsEnabled": false,
+ "galleryDecorNodesEnabled": false,
+ "zoomEnabled": true,
+ "cloud": false,
+ "singleFile": false,
+ "title": "Portfolio-Diana-Teodoro",
+ "layersExist": false,
+ "pages": [
+  {
+   "id": "1:944",
+   "groupIndex": 0,
+   "title": "Landing",
+   "image": "landing.png",
+   "index": 0,
+   "width": 393,
+   "height": 2388,
+   "x": 523,
+   "y": 2402,
+   "awidth": 393,
+   "aheight": 2388,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Frame 649",
+     "rect": {
+      "x": 0,
+      "y": 755,
+      "width": 393,
+      "height": 480
+     },
+     "index": 0,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 1,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Frame 4",
+     "rect": {
+      "x": 42,
+      "y": 1087,
+      "width": 140,
+      "height": 48
+     },
+     "index": 1,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 1,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Frame 650",
+     "rect": {
+      "x": 0,
+      "y": 1235,
+      "width": 393,
+      "height": 536
+     },
+     "index": 2,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Frame 4",
+     "rect": {
+      "x": 42,
+      "y": 1623,
+      "width": 185,
+      "height": 48
+     },
+     "index": 3,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Frame 651",
+     "rect": {
+      "x": 0,
+      "y": 1771,
+      "width": 393,
+      "height": 508
+     },
+     "index": 4,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 6,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Frame 4",
+     "rect": {
+      "x": 42,
+      "y": 2131,
+      "width": 150,
+      "height": 48
+     },
+     "index": 5,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 6,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "1:983",
+   "groupIndex": 0,
+   "title": "my design principles main",
+   "image": "my-design-principles-main.png",
+   "index": 1,
+   "width": 393,
+   "height": 4972,
+   "x": 1039,
+   "y": 2402,
+   "awidth": 393,
+   "aheight": 4972,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Frame 659",
+     "rect": {
+      "x": 0,
+      "y": 4755,
+      "width": 196.5,
+      "height": 109
+     },
+     "index": 6,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Frame 658",
+     "rect": {
+      "x": 196.5,
+      "y": 4755,
+      "width": 196.5,
+      "height": 109
+     },
+     "index": 7,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 6,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Footer",
+     "rect": {
+      "x": 0,
+      "y": 4864,
+      "width": 393,
+      "height": 109
+     },
+     "index": 8,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "1:1037",
+   "groupIndex": 0,
+   "title": "recent projects main",
+   "image": "recent-projects-main.png",
+   "index": 2,
+   "width": 393,
+   "height": 959,
+   "x": 1558,
+   "y": 2402,
+   "awidth": 393,
+   "aheight": 959,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Qualitatively, Blendy",
+     "rect": {
+      "x": 42,
+      "y": 259,
+      "width": 309,
+      "height": 76
+     },
+     "index": 9,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Content Discovery",
+     "rect": {
+      "x": 42,
+      "y": 411,
+      "width": 309,
+      "height": 76
+     },
+     "index": 10,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 4,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Designing for TV",
+     "rect": {
+      "x": 42,
+      "y": 577,
+      "width": 309,
+      "height": 76
+     },
+     "index": 11,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Frame 659",
+     "rect": {
+      "x": 0,
+      "y": 742,
+      "width": 196.5,
+      "height": 109
+     },
+     "index": 12,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 1,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Frame 658",
+     "rect": {
+      "x": 196.5,
+      "y": 742,
+      "width": 196.5,
+      "height": 109
+     },
+     "index": 13,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 6,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Footer",
+     "rect": {
+      "x": 0,
+      "y": 851,
+      "width": 393,
+      "height": 109
+     },
+     "index": 14,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "1:1120",
+   "groupIndex": 0,
+   "title": "Qualitatively Blendy M",
+   "image": "qualitatively-blendy-m.png",
+   "index": 3,
+   "width": 393,
+   "height": 9944,
+   "x": 2077,
+   "y": 2402,
+   "awidth": 393,
+   "aheight": 9944,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Footer",
+     "rect": {
+      "x": 0,
+      "y": 9617.275390625,
+      "width": 393,
+      "height": 109
+     },
+     "index": 15,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 4,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Frame 4",
+     "rect": {
+      "x": 42,
+      "y": 80,
+      "width": 185,
+      "height": 48
+     },
+     "index": 16,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Frame 659",
+     "rect": {
+      "x": 0,
+      "y": 9726.275390625,
+      "width": 196.5,
+      "height": 109
+     },
+     "index": 17,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 1,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Frame 658",
+     "rect": {
+      "x": 196.5,
+      "y": 9726.275390625,
+      "width": 196.5,
+      "height": 109
+     },
+     "index": 18,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 6,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Footer",
+     "rect": {
+      "x": 0,
+      "y": 9835.275390625,
+      "width": 393,
+      "height": 109
+     },
+     "index": 19,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "1:1215",
+   "groupIndex": 0,
+   "title": "Content Discovery M",
+   "image": "content-discovery-m.png",
+   "index": 4,
+   "width": 393,
+   "height": 7737,
+   "x": 2551,
+   "y": 2402,
+   "awidth": 393,
+   "aheight": 7737,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Footer",
+     "rect": {
+      "x": 0,
+      "y": 7410.71875,
+      "width": 393,
+      "height": 109
+     },
+     "index": 20,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Frame 659",
+     "rect": {
+      "x": 0,
+      "y": 7519.71875,
+      "width": 196.5,
+      "height": 109
+     },
+     "index": 21,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 1,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Frame 658",
+     "rect": {
+      "x": 196.5,
+      "y": 7519.71875,
+      "width": 196.5,
+      "height": 109
+     },
+     "index": 22,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 6,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Footer",
+     "rect": {
+      "x": 0,
+      "y": 7628.71875,
+      "width": 393,
+      "height": 109
+     },
+     "index": 23,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Frame 4",
+     "rect": {
+      "x": 42,
+      "y": 80,
+      "width": 185,
+      "height": 48
+     },
+     "index": 24,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "1:1291",
+   "groupIndex": 0,
+   "title": "Designing for TV M",
+   "image": "designing-for-tv-m.png",
+   "index": 5,
+   "width": 393,
+   "height": 8809,
+   "x": 3025,
+   "y": 2402,
+   "awidth": 393,
+   "aheight": 8809,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Frame 4",
+     "rect": {
+      "x": 42,
+      "y": 80,
+      "width": 185,
+      "height": 48
+     },
+     "index": 25,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Footer",
+     "rect": {
+      "x": 0,
+      "y": 8482,
+      "width": 393,
+      "height": 109
+     },
+     "index": 26,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Frame 659",
+     "rect": {
+      "x": 0,
+      "y": 8591,
+      "width": 196.5,
+      "height": 109
+     },
+     "index": 27,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 1,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Frame 658",
+     "rect": {
+      "x": 196.5,
+      "y": 8591,
+      "width": 196.5,
+      "height": 109
+     },
+     "index": 28,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 6,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Footer",
+     "rect": {
+      "x": 0,
+      "y": 8700,
+      "width": 393,
+      "height": 109
+     },
+     "index": 29,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "1:1088",
+   "groupIndex": 0,
+   "title": "contact me M",
+   "image": "contact-me-m.png",
+   "index": 6,
+   "width": 393,
+   "height": 1434,
+   "x": 3541,
+   "y": 2402,
+   "awidth": 393,
+   "aheight": 1434,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Footer",
+     "rect": {
+      "x": 0,
+      "y": 1325,
+      "width": 393,
+      "height": 109
+     },
+     "index": 30,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 6,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Frame 659",
+     "rect": {
+      "x": 0,
+      "y": 1216,
+      "width": 196.5,
+      "height": 109
+     },
+     "index": 31,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 6,
+       "frameIndex": 1,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Frame 658",
+     "rect": {
+      "x": 196.5,
+      "y": 1216,
+      "width": 196.5,
+      "height": 109
+     },
+     "index": 32,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 6,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  }
+ ],
+ "totalImages": 7,
+ "fileKey": "portfolio10octmobile",
+ "groups": [
+  {
+   "id": "0:1",
+   "index": 0,
+   "name": "Adapted Prototype",
+   "backColor": "#2F2F2F"
+  }
+ ]
+}
